61fe529f6bf883a9f7be9d0caa27623bad1b0080
Cayden <joking4joker@gmail.com>
2023-12-29 21:29:40 +0000

    [backend]: updated firebase api

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: ...,
-    authDomain: ...,
-    projectId: ...,
-    storageBucket: ...,
-    messagingSenderId: ...,
-    appId: ...,
-    measurementId: ...
+    apiKey: "AIzaSyD3W4pXLVayLT0zQ13M-YGX564YeGragp0",
+    authDomain: "healthmate-c8e5c.firebaseapp.com",
+    projectId: "healthmate-c8e5c",
+    storageBucket: "healthmate-c8e5c.appspot.com",
+    messagingSenderId: "715228782698",
+    appId: "1:715228782698:web:a42075c92fa22db03aea13",
+    measurementId: "G-4SB0X37N3G",
 };
 
 const app = initializeApp(firebaseConfig);
