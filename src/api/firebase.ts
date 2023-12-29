b7ba299ac5c96c9b8c68f679ca546d716fb9a261
Cayden <joking4joker@gmail.com>
2023-12-29 21:27:56 +0000

    [backend]: Firebase config

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
     storageBucket: "healthmate-c8e5c.appspot.com",
     messagingSenderId: "715228782698",
     appId: "1:715228782698:web:a42075c92fa22db03aea13",
-    measurementId: "G-4SB0X37N3G"
+    measurementId: "G-4SB0X37N3G",
 };
 
 const app = initializeApp(firebaseConfig);
