f2f16d84fbb46c766c8dcf695b6dcf2fb3f25280
Cayden <joking4joker@gmail.com>
2023-12-30 17:35:58 +0000

    [refactor]: Updated firebase.ts

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD3W4pXLVayLT0zQ13M-YGX564YeGragp0",
-    authDomain: "healthmate-c8e5c.firebaseapp.com",
-    projectId: "healthmate-c8e5c",
-    storageBucket: "healthmate-c8e5c.appspot.com",
-    messagingSenderId: "715228782698",
-    appId: "1:715228782698:web:a42075c92fa22db03aea13",
-    measurementId: "G-4SB0X37N3G",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
